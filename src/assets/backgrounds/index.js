3138e25f6938c2d922c0f3d728818a130040af82
Kyle Joseph <kyle.joseph@footballwhispers.com>
2018-11-11 22:06:49 +0000

    delete

--- src/assets/backgrounds/index.js
+++
@@ -1,9 +0,0 @@
-import BackgroundGradient from './game/background-with-planets.png';
-import BackgroundClouds from './game/clouds.png';
-import BackgroundSea from './game/mountain_and_sea.png';
-
-export default {
-  BackgroundClouds,
-  BackgroundClouds,
-  BackgroundSea
-};
--- src/assets/levels/raw/level-0/input-tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="input-tiles" tilewidth="16" tileheight="16" tilecount="8" columns="8">
- <image source="input-tiles.png" width="128" height="16"/>
-</tileset>
